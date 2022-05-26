b42c82915494dde3d9359a6f3441d2567855958f
Aiden <hyena.shenanigans@gmail.com>
2022-05-25 22:28:28 -0700

    Fixed data display mixup

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import MovieMovie from './MovieMovie'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
 
 class Movie extends React.Component {
-    render() {
-      return (
-        <>{this.props.movieReport &&
-          <div>{this.props.movieReport.map(value => (<MovieMovie title={value.title} description={value.description} avgVotes={value.avgVotes} totalVotes={value.totalVotes} popularity={value.popularity} release={value.release} poster={value.poster} alt={value.title}></MovieMovie>))}</div>
-        }
-        </>
-      )
-    }
+  render() {
+    return (
+      <>
+      <Container>
+        <Row sm={3} md={4} lg={5}>
+          {this.props.movieReport &&
+            <div>{this.props.movieReport.map(value => (<MovieMovie key={value.title} title={value.title} description={value.description} avgVotes={value.avgVotes} totalVotes={value.totalVotes} popularity={value.popularity} release={value.release} poster={value.poster} alt={value.title}></MovieMovie>))}</div>
+          }
+        </Row>
+        </Container>
+      </>
+    )
   }
+}
 
 export default Movie;
--- src/WeatherDay.js
+++ src/WeatherDay.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 
 class WeatherDay extends React.Component {
     render() {
       return (
-        <Card>
-        <Card.Title>{this.props.title}</Card.Title>
-        <Card.Text>{this.props.description}</Card.Text>
-        <Card.Text>Average Votes:{this.props.avgVotes}</Card.Text>
-        <Card.Text>totalVotes={this.props.totalVotes}</Card.Text>
-        <Card.Text>popularity={this.props.popularity}</Card.Text>
-        <Card.Text>release={this.props.release} poster={this.props.poster}</Card.Text>
-        <Card.Text>alt={this.props.title}</Card.Text>
-        </Card>
+        <Col>
+        <div>Date: {this.props.dateTime} | Forecast: {this.props.description}</div>
+        </Col>
       )
     }
   }
